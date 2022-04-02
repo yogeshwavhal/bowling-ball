9252f96abd9cf0cde21f6390edfb431231c9f529
AD001\z004aruj <yogesh.wavhal@siemens.com>
2022-04-03 02:41:49 +0530

    Add project files.

---
+++ BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+        #region Private Fields
+
+        private Game _game;
+
+        #endregion
+
+        #region Public Methods
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+
+        [TestMethod]
+        public void Gutter_game_score_should_be_zero_test()
+        {
+            Roll(_game, 0, 20);
+            Assert.AreEqual(0, _game.GetScore());
+        }
+
+        [TestMethod]
+        public void AllRoleWithOnePin_ShouldReturnScoreAs_20_test()
+        {
+            // Arrange - done in SetUp
+
+            // Action
+            Roll(_game, 1, 20);
+
+            // Assertion
+            Assert.AreEqual(20, _game.GetScore());
+        }
+
+        [TestMethod]
+        public void OneSpare_ShouldReturnScoreAs_14_test()
+        {
+            RoleSpare();
+            _game.Roll(2);
+
+            // Total score should be 5 + 5 + 3 (Bonus of next frame) + 3 = 16
+            // Role limit should be 17 becuase we have spare here and to calculate the bonus we need next frame score
+            // So 17 as limit
+            Roll(_game, 0, 17);
+
+            Assert.AreEqual(14, _game.GetScore());
+        }
+
+        [TestMethod]
+        public void OneStrike_ShouldReturnScoreAs_24_test()
+        {
+            RoleStrike();
+            _game.Roll(3);
+            _game.Roll(4);
+
+            // Total score should be 10 + 3 + 4 (Bonus of next frame) + 3 + 4 = 24
+            Roll(_game, 0, 16);
+
+            Assert.AreEqual(24, _game.GetScore());
+        }
+
+        [TestMethod]
+        public void PerfectGame_ShouldReturnScoreAs_300_test()
+        {
+            Roll(_game, 10, 12);
+            Assert.AreEqual(300, _game.GetScore());
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Roll(Game game, int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        private void RoleStrike()
+        {
+            _game.Roll(10);
+        }
+
+        private void RoleSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+        }
+
+        #endregion
+    }
+}
---
+++ BowlingBall/Game.cs
@@ -0,0 +1,88 @@
+﻿
+namespace BowlingBall
+{
+    public class Game
+    {
+        #region Private Fields
+
+        private int[] _rolls;
+        private int _currentRoll = 0;
+
+        #endregion
+
+        #region Public Constructor
+
+        public Game()
+        {
+            // for 10th frame we can get more rolls we have strike or spare
+            _rolls = new int[21];
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public int GetScore()
+        {
+            var score = 0;
+            int roleIndex = 0;
+            // consider frame which has two rolls
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(roleIndex))
+                {
+                    score += StrikeBonus(roleIndex);
+                    roleIndex++;
+                }
+                else if (IsSpare(roleIndex))
+                {
+                    score += 10 + SpareBonus(roleIndex);
+                    roleIndex += 2;
+                }
+                else
+                {
+                    score += BallSum(roleIndex);
+                    roleIndex += 2;
+                }
+
+            }
+            return score;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private int BallSum(int roleIndex)
+        {
+            return _rolls[roleIndex] + _rolls[roleIndex + 1];
+        }
+
+        private int SpareBonus(int roleIndex)
+        {
+            return _rolls[roleIndex + 2];
+        }
+
+        private int StrikeBonus(int roleIndex)
+        {
+            return 10 + _rolls[roleIndex + 1] + _rolls[roleIndex + 2];
+        }
+
+        private bool IsStrike(int roleIndex)
+        {
+            return _rolls[roleIndex] == 10;
+        }
+
+        private bool IsSpare(int roleIndex)
+        {
+            return _rolls[roleIndex] + _rolls[roleIndex + 1] == 10;
+        }
+
+        public void Roll(int pins)
+        {
+            _rolls[_currentRoll++] = pins;
+        }
+
+        #endregion
+    }
+}
